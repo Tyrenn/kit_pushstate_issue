68f592cbd4d8d8b142e0532e83be1573bcee6264
Tyrenn <tyren.nawak@gmail.com>
2024-02-07 10:37:34 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import {pushState, replaceState} from '$app/navigation';
+	import { page } from '$app/stores';
+	import {onMount} from 'svelte';
+
+	let showSquare = false;
+
+	function onChangeState(state){
+		console.log("change state", state)
+		if(state?.show)
+			showSquare = true;
+		else
+			showSquare = false;
+	}
+
+	function onShow(){
+		pushState('/square', {show : true});
+	}
+
+	function onHide(){
+		pushState('/', {show : false});
+	}
+
+	$ : onChangeState($page.state)
+
+	onMount(() => {
+
+	})
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<svelte:window onpopstate={() => console.log('Popping state')} />
+
+<section>
+	<button on:click={onShow}>Show Square</button>
+	<button on:click={onHide}>Hide Square</button>
+
+
+	{#if showSquare}
+		<div class="square"/>
+	{/if}
+
+</section>
+
+<style>
+
+	.square{
+		width: 40px;
+		height: 40px;
+		background-color: red;
+		margin-top: 20px;
+	}
+
+</style>
